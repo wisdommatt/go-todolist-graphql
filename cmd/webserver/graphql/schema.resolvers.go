c9ee18e29fcdc8d282169d069c74afff6f0508b0
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-08-12 19:24:43 +0100

    feat: init go.mod with gqlgen

---
+++ cmd/webserver/graphql/schema.resolvers.go
@@ -0,0 +1,30 @@
+//go:generate go run github.com/99designs/gqlgen
+package graphql
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/wisdommatt/go-todolist-graphql/cmd/webserver/graphql/generated"
+	"github.com/wisdommatt/go-todolist-graphql/cmd/webserver/graphql/model"
+)
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
