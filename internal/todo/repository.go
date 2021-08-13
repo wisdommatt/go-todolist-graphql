8c66fb5fac3165de93d98375857c7d15eefb3540
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-08-13 10:06:02 +0100

    feat(todo): add `GetTodos` method to todo repository

--- internal/todo/repository.go
+++ internal/todo/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 // Repository is the interface that describes a todo repository object.
 type Repository interface {
 	Save(todo *Todo) error
+	GetTodos(status string) (todos []Todo, err error)
 }
 
 // TodoRepo is the default implementation for the repository interface.
@@ -23,4 +24,14 @@ func NewRepository(db *gorm.DB) *TodoRepo {
 func (repo *TodoRepo) Save(todo *Todo) error {
 	repo.db.Create(todo)
 	return nil
+}
+
+// GetTodos retrieves todos from the database by filter.
+func (repo *TodoRepo) GetTodos(status string) (todos []Todo, err error) {
+	if status != "" {
+		repo.db.Where("status = ?", status).Find(&todos)
+		return todos, nil
+	}
+	repo.db.Find(&todos)
+	return todos, nil
 }
